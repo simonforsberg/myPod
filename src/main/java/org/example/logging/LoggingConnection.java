4c4edc0785f0f010176fa646b441a4ae428a678b
Jesper Larsson <jesper.larsson@iths.se>
2026-01-14 23:44:35 +0100

    connection pool for logging
    Signed-off-by: Jesper Larsson <jesper.larsson@iths.se>

---
+++ src/main/java/org/example/logging/LoggingConnection.java
@@ -0,0 +1,29 @@
+package org.example.logging;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class LoggingConnection {
+
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
+
+    static {
+        config.setJdbcUrl( "jdbc:mysql://localhost:3306/myPodDB" );
+        config.setUsername( "user" );
+        config.setPassword( "pass" );
+        config.addDataSourceProperty( "cachePrepStmts" , "true" );
+        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
+        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
+        ds = new HikariDataSource( config );
+    }
+
+    private LoggingConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+}
